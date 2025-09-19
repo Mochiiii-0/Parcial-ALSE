6309e96e9664c75824f226c144be9e0101fa0541
Mochi <sara.otero4042@gmail.com>
2025-09-19 10:57:46 -0500

    incluir la funcion para calcular la distancia

--- Parcial_primertercio.cpp
+++ Parcial_primertercio.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <cmath>
 
+// Defino Double para almacenar coordenadas (x, y)
 struct Point {
     double x, y;
 };
+
+// Función para calcular la distancia entre dos puntos.
+double calcularDistancia(const Point &p1, const Point &p2)
+{
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return std::sqrt(dx * dx + dy * dy);
+}
+
